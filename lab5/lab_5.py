62cadd556b5a5424dbd8ec8130101e0244d5c1f2
Oleg Baskakov <spetz911@gmail.com>
2011-11-08 23:05:00 +0400

    all in one file

--- lab5/lab_5.py
+++ lab5/lab_5.py
@@ -1,15 +1,6 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from math import sqrt,sin,cos,tan,pi,log,exp
-from copy import copy, deepcopy
-from functools import reduce
-eps = 0.0001
-
-from pde import *
-
-def frange(x0, x1, d):
-	return [ x0 + d*k for k in range(int((x1-x0+eps/10)/d)+1)]
 
 def print_mat(mat):
 	print("matrix:")
@@ -25,31 +16,6 @@ def print_vec(row):
 		print(round(elem, 4), end = "\t")
 	print()
 	print("--------")
-
-class Tridiagonal_Matrix:
-	def solve(self):
-		"""Method progonki"""
-		a = self.a
-		b = self.b
-		c = self.c
-		d = self.d
-		n = self.n
-		
-		P = []
-		Q = []
-		P.append(-c[0]/b[0])
-		Q.append( d[0]/b[0])
-		
-		for i in range(1, n):
-			P.append( -c[i] / (b[i]+a[i]*P[i-1]) ) 
-			Q.append( (d[i] - a[i]*Q[i-1]) / (b[i] + a[i]*P[i-1]) )
-
-		x = [0]*n
-		x[n-1] = Q[n-1]
-		for i in range(n-2, -1, -1):
-			x[i] = P[i]*x[i+1] + Q[i]
-			
-		return x
 
 
 
@@ -71,138 +37,11 @@ U2 = [u0]
 
 
 
-class PDE_parser():
-	def __init__(self):
-		pass
-	
-	## Description: for parsing one element of summ
-	@staticmethod
-	def get_token(x):
-		tmp = x.split('*')
-		if tmp[-1] in ['u', 'u_xx', 'u_x', 'u_yy', 'u_y', 'u_tt', 'u_t']:
-			try:
-				a = float(tmp[0])
-			except:
-				a = 1.0
-			return (tmp[-1], a)
-		else:
-			return None
-
-	def parse_pde(self, s):
-		"""For parsing partial differential equation"""
-		[left, right] = s.lower().rstrip().replace(' ','').split('=')
-		self.type = None
-		if ('u_xx' in right) and ('u_t' in left):
-			self.type = 'parabolic'
-		elif ('u_xx' in right) and ('u_tt' in left):
-			self.type = 'hyperbolic'
-		elif ('u_xx' in right) and ('u_yy' in right):
-			self.type = 'elliptic'
-
-		tokens = [ x for x in left.split('+') + right.split('+')]
-		rest = []
-		for x in tokens:
-			t = PDE_parser.get_token(x)
-			if t:
-				self.__setattr__(t[0], t[1])
-			else:
-				rest.append(x)
-		try: self.fun = eval("lambda x,t: " + "+".join(rest))
-		except: self.fun = lambda x,t: 0.0
-
-	def parse_stuff(self, line):
-		"""For parsing other coefficients"""
-		[left, right] = line.lower().replace(' ','').split('=')
-		if left in ['l', 'lx', 'ly', 't', 'w', 'eps']:
-			self.__setattr__(left, float(right))
-
-	## Description: for solve init eq
-	def parse_initial_condition(self, line):
-		[left, right] = line.lower().replace(' ','').split('=')
-		if left in ['u(x,0)', 'u(x,y,0)', 'u(x,y)']:
-			try: self.init = eval("lambda x,y=0: " + right)
-			except: self.init = lambda x,y=0: 0.0
-		if left in ['u_t(x,0)', 'u_t(x,y,0)', 'u_t(x,y)']:
-			try: self.init1 = eval("lambda x,y=0: " + right)
-			except: self.init1 = lambda x,y=0: 0.0
-
-	## Description: for solve limit eq
-	def parse_boundary_condition(self, line):
-		[left, right] = line.lower().replace(' ','').split('=')
-		#! alpha*u_x + beta*u = f(t)
-		alpha = 0.0
-		beta = 0.0
-		
-		for elem in left.split('+'):
-			tmp = elem.split('*')
-			if ('u_x' in tmp[-1] or 'u_y' in tmp[-1]):
-				try: alpha = float(tmp[0])
-				except: alpha = 1.0
-			elif ('u' in tmp[-1]):
-				try: beta = float(tmp[0])
-				except: beta = 1.0
-		try: result = (alpha, beta, eval("lambda t: " + right))
-		except: result = (alpha, beta, lambda t: 0.0)
-
-		if   ('(x,ly' in left):
-			self.north = result
-		elif ('(0,x' in left):
-			self.south = result
-		elif ('(l' in left): # (lx,y,0)
-			self.east = result
-		elif ('(0' in left): # (0,y,0)
-			self.west = result
-		
-		try: self.left = self.west
-		except: pass
-		try: self.right = self.east
-		except: pass
 
 
 
-
-s = "15*u_t = 0.1*u_xx + 0.2*u_x + -3.0*u + x*t"
-
-
-
-##===========================================================================
-## Description: parse all input
-def parse_all(s):
-	print("s:", s)
-	pde = PDE_parser()
-	pde.parse_pde(s)
-	print("pde =")
-	print(pde.type)
-	if pde.type == 'parabolic':
-		print("f(1,1) =", pde.fun(1,1))
-		eq = PDE_parser()
-		
-		
-	elif pde.type == 'hyperbolic':
-		pass
-	elif pde.type == 'elliptic':
-		pass
-	
-	
-	return None
+s0 = "15*u_t = 0.1*u_xx + 0.2*u_x + -3.0*u + x*t"
 
 
 
-
-
-
-
-#============================================================================
-
-def main():
-
-	s = open("input").readline()
-	
-	print(parse_all(s))
-
-
-#=====================================================================
-
-if __name__ == "__main__":
-	main()
 
--- lab5/pde.py
+++ lab5/pde.py
@@ -1,56 +1,208 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from math import sqrt,sin,cos,tan,pi,log,exp
+from copy import copy, deepcopy
+from functools import reduce
+
+from pprint import pprint
+
+eps = 0.0001
+
+
+class PDE_parser():
+	def __init__(self):
+		pass
+	
+	## Description: for parsing one element of summ
+	@staticmethod
+	def get_token(x):
+		tmp = x.split('*')
+		if tmp[-1] in ['u', 'u_xx', 'u_x', 'u_yy', 'u_y', 'u_tt', 'u_t']:
+			try:
+				a = float(tmp[0])
+			except:
+				a = 1.0
+			return (tmp[-1], a)
+		else:
+			return None
+
+	def parse_pde(self, s):
+		"""For parsing partial differential equation"""
+		[left, right] = s.lower().rstrip().replace(' ','').split('=')
+		self.type = None
+		if ('u_xx' in right) and ('u_t' in left):
+			self.type = 'parabolic'
+		elif ('u_xx' in right) and ('u_tt' in left):
+			self.type = 'hyperbolic'
+		elif ('u_xx' in right) and ('u_yy' in right):
+			self.type = 'elliptic'
+
+		tokens = [ x for x in left.split('+') + right.split('+')]
+		rest = []
+		for x in tokens:
+			t = PDE_parser.get_token(x)
+			if t:
+				self.__setattr__(t[0], t[1])
+			else:
+				rest.append(x)
+		try: self.fun = eval("lambda x,t: " + "+".join(rest))
+		except: self.fun = lambda x,t: 0.0
+
+	def parse_stuff(self, line):
+		"""For parsing other coefficients"""
+		[left, right] = line.lower().replace(' ','').split('=')
+		if left in ['l', 'lx', 'ly', 't', 'w', 'eps', 'tau', 'h']:
+			self.__setattr__(left, float(right))
+
+	## Description: for solve init eq
+	def parse_initial_condition(self, line):
+		[left, right] = line.lower().replace(' ','').split('=')
+		if left in ['u(x,0)', 'u(x,y,0)', 'u(x,y)']:
+			try: self.initial0 = eval("lambda x,y=0: " + right)
+			except: self.initial0 = lambda x,y=0: 0.0
+		if left in ['u_t(x,0)', 'u_t(x,y,0)', 'u_t(x,y)']:
+			try: self.initial1 = eval("lambda x,y=0: " + right)
+			except: self.initial1 = lambda x,y=0: 0.0
+
+	## Description: for solve limit eq
+	def parse_boundary_condition(self, line):
+		[left, right] = line.lower().replace(' ','').split('=')
+		#! alpha*u_x + beta*u = f(t)
+		alpha = 0.0
+		beta = 0.0
+		
+		for elem in left.split('+'):
+			tmp = elem.split('*')
+			if ('u_x' in tmp[-1] or 'u_y' in tmp[-1]):
+				try: alpha = float(tmp[0])
+				except: alpha = 1.0
+			elif ('u' in tmp[-1]):
+				try: beta = float(tmp[0])
+				except: beta = 1.0
+		try: result = (alpha, beta, eval("lambda t: " + right))
+		except: result = (alpha, beta, lambda t: 0.0)
+
+		if   ('(x,ly' in left):
+			self.north = result
+		elif ('(0,x' in left):
+			self.south = result
+		elif ('(l' in left): # (lx,y,0)
+			self.east = result
+		elif ('(0' in left): # (0,y,0)
+			self.west = result
+		
+		try: self.left = self.west
+		except: pass
+		try: self.right = self.east
+		except: pass
+
+
+class Tridiagonal_Matrix:
+	def solve(self):
+		"""Method progonki"""
+		a = self.a
+		b = self.b
+		c = self.c
+		d = self.d
+		n = self.n
+		
+		P = []
+		Q = []
+		P.append(-c[0]/b[0])
+		Q.append( d[0]/b[0])
+		
+		for i in range(1, n):
+			P.append( -c[i] / (b[i]+a[i]*P[i-1]) ) 
+			Q.append( (d[i] - a[i]*Q[i-1]) / (b[i] + a[i]*P[i-1]) )
+
+		x = [0]*n
+		x[n-1] = Q[n-1]
+		for i in range(n-2, -1, -1):
+			x[i] = P[i]*x[i+1] + Q[i]
+			
+		return x
+
+
+class MetaClass:
+	def copy(self, src):
+		[self.__setattr__(k,v) for k,v in src.__dict__.items()]
+	def print(self):
+		from pprint import pprint
+		pprint(dict(self.__dict__))
+
+eps = 0.00001
+def frange(x0, x1, d):
+	while (x0 < x1 + eps):
+		yield x0
+		x0 += d
+
+
 
 class PDE:
+	def __init__(self, pde = None):
+		print("PDE constructor", pde)
+		if pde:
+			MetaClass.copy(self, pde)
+		else:
+			pass
+
+	@staticmethod
 	def scalar(v1, v2):
 		return sum(x1*x2 for (x1,x2) in zip(v1,v2))
-	
-	def mat_vec(m, v):
-		return [scalar(vx, v) for vx in m]
 
+	@staticmethod
+	def mat_vec(m, v):
+		return [PDE.scalar(vx, v) for vx in m]
+
+	@staticmethod
 	def vec_mat(v, m):
-		return mat_vec(zip(*m), v)
+		return PDE.mat_vec(zip(*m), v)
+
 	
-	## Description: solve equation
-	def explicit_method(u, u_k_1 = 0): #TODO add threads
-		sigma = tau * alpha**2 / h**2
-		omega = tau * b / (2*h)
-		eta = c * tau
-		# move it to inheritance class
-		coef_a = [+1, -2, +1]
-		coef_b = [-3, +4, -1]
-		coef_c = [+0, +1, +0]
+	## Description: 
+	def explicit_method(self): #TODO add threads
+		"""Just solve equation"""
+		U = self.grid[-1]
+		N = len(U)
+
+		sigma = self.sigma
+		omega = self.omega
+		eta = self.eta
 		
-		coef_mat = vec_mat([sigma, omega, eta], [coef_a, coef_b, coef_c])
+		coef_mat = PDE.vec_mat([self.sigma, self.omega, self.eta],
+		                       [self.coef_a, self.coef_b, self.coef_c])
+		# TODO f(x,t) != 0
+#		print(list(zip(* [self.coef_a, self.coef_b, self.coef_c])))
 		
-		res = [0] + [mat_vec(coef_mat, u[i-1:(i+1)+1]) for i in range(1, N-1)] + [0]
+		print(coef_mat)
 		
-		(a0, b0, c0, d0) = first_eq(u)
-		(an, bn, cn, dn) = last_eq(u)
+		res = [0] + [PDE.scalar(coef_mat, U[i-1:(i+1)+1]) for i in range(1, N-1)] + [0]
+		
+		(a0, b0, c0, d0) = self.first_eq(U)
+		(an, bn, cn, dn) = self.last_eq(U)
 		res[0]  = (d0 - c0*res[1])  / b0
 		res[-1] = (dn - bn*res[-2]) / an
 	
 		return res
 
-	## Description: solve with method 'Progonki'
-	def implicit_method():
+	def implicit_method(self, u):
+		"""Solve with method Progonki"""
 		N = len(u)
 	
-		M = Tridiagonal_Matrix();
+		M = Tridiagonal_Matrix()
 	
-		M = zip(* first_eq(pde) + middle_eq(pde) * (N-2) + last_eq(pde) )
+		M = zip(* self.first_eq() + self.middle_eq() *(N-2) + self.last_eq())
 	
-
 		M.n = N
 	
 		print_mat([M.a, M.b, M.c, M.d])
 
 		x = M.solve()
-		print_vec( [u0] + x + [u1])
+		print_vec([u0] + x + [u1])
 
 	## Description: solve with method 'Progonki'
-	def Crank_Nicolson_method():
+	def Crank_Nicolson_method(self):
 		pass
 		# a_i /= Q 
 		# b_i /= Q 
@@ -59,66 +211,113 @@ class PDE:
 	
 
 
-
-
-
 class Parabolic_PDE(PDE):
-	def __init__(pde = dict()):
-		pde.__init__(pde)
+	u_x = 0.0
+	u = 0.0
+	coef_a = [+1, -2, +1]
+	coef_b = [-3, +4, -1]
+	coef_c = [+0, +1, +0]
+	
+	def __init__(self, pde = None):
+		super(Parabolic_PDE, self).__init__(pde)
 		
+		MetaClass.print(self)
+
+		a = self.u_xx
+		b = self.u_x
+		c = self.u
+		h = self.h
+		l = self.l
+
+		tau = self.tau
+		
+		self.sigma = tau * a**2 / h**2
+		self.omega = tau * b / (2*h)
+		self.eta = c * tau # TODO add f(x,t)
+		
+		psi0 = self.initial0
+		U = []
+		U.append([ psi0(x) for x in frange(0, l, h)])
+		self.grid = U
 	
-	def first_eq(pde):
+	
+	def first_eq(self, t):
 		"""Find coefficients of first equation"""
-#		alpha = pde['init'][0]
-#		beta  = pde['init'][1]
-#		phi1  = pde['init'][2]
-		a = pde['u_xx']
-		b = pde['u_x']
-		c = pde['u']
-	
+		alpha = self.left[0]
+		beta  = self.left[1]
+		phi0  = self.left[2]
+		U = self.grid[-1]
+
+		a = self.u_xx
+		b = self.u_x
+		c = self.u
+		h = self.h
+		tau = self.tau
+		
 		a0 = 0
-		b0 = self.alpha * (2*a*a/h + h/tau - c*h) - self.beta * (2*a*a - b*h)
-		c0 = self.alpha * (2*a*a/h)
-		d0 = self.alpha * (u[0][k] * h/tau) - self.phi1(tau*(k+1)) * (2*a*a - b*h)
+		b0 = alpha * (2*a*a/h + h/tau - c*h) - beta * (2*a*a - b*h)
+		c0 = alpha * (2*a*a/h)
+		d0 = alpha * (U[0] * h/tau) - phi0(t) * (2*a*a - b*h)
 		return (a0, b0, c0, d0)
 	
-	def last_eq(u):
+	def last_eq(self, t):
 		"""Find coefficients of last equation"""
-#		alpha = pde['init1'][0]
-#		beta  = pde['init1'][1]
-#		phi1  = pde['init1'][2]
-		a = pde['u_xx']
-		b = pde['u_x']
-		c = pde['u']
+		alpha = self.right[0]
+		beta  = self.right[1]
+		phi1  = self.right[2]
+		U = self.grid[-1]
+		N = len(U)
 
-		an = self.alpha * (-2*a*a/h)
-		bn = self.alpha * (2*a*a/h + h/tau - c*h) + self.beta * (2*a*a + b*h)
+		a = self.u_xx
+		b = self.u_x
+		c = self.u
+		h = self.h
+		tau = self.tau
+
+		an = alpha * (-2*a*a/h)
+		bn = alpha * (2*a*a/h + h/tau - c*h) + beta * (2*a*a + b*h)
 		cn = 0
-		dn = self.alpha * (u[N][k] * h/tau) + self.phi1(tau*(k+1)) * (2*a*a + b*h)
+		dn = alpha * (U[-1] * h/tau) + phi1(t) * (2*a*a + b*h)
 		return (an, bn, cn, dn)
 
-	## Description: find coefficients of last equation
 	def middle_eq(u):
 		"""Find coefficients of middle equation"""
-		a = pde['u_xx']
-		b = pde['u_x']
-		c = pde['u']
-	
-		sigma = a*a * tau / (h*h)
-		omega = b * tau / (2*h)
-		eta   = c*tau #TODO add f(x,t) here
-	
-		ai = sigma - 3*omega
-		bi = 1 - 2*sigma + 4*omega + eta
-		ci = sigma - omega
+		ai = sum(koef_a)
+		bi = sum(koef_b)
+		ci = sum(koef_c)
 		di = -u[N][k]
 
 		return (ai, bi, ci, di)
+	
+	def solve(self):
+		U = self.grid
+		
+		for t in frange(0, self.t, self.tau):
+			U.append(self.explicit_method())
+		
+		print("ololo")
+		return U
+	
 
 class Hyperbolic_PDE(PDE):
 	def __init__():
 		pde.__init__()
-		pass
+		self.sigma = tau**2 * a**2 / h**2
+		self.omega = tau**2 * b / (2*h)
+		self.eta = c * tau**2 + 2 # TODO add f(x,t)
+
+		U = []
+		psi0 = self.initial0
+		psi1 = self.initial1
+		
+		U.append([ psi0(x) for x in frange(0, T, t)])
+		U.append([ psi0(x) + psi1(x)*tau for x in frange(0, T, t)])
+		for k in range(1, N-1):
+			U[-1][k] += (U[0][k-1] - 2*U[0][k] + U[0][k+1]) * alpha**2 * tau**2 / (2 * h**2)
+ 		
+		self.grid = U
+
+
 	
 	## Description: find coefficients of last equation
 	def middle_eq(u):
@@ -127,16 +326,14 @@ class Hyperbolic_PDE(PDE):
 		b = pde['u_x']
 		c = pde['u']
 	
-		sigma = tau**2 * alpha**2 / h**2
-		omega = tau**2 * b / (2*h)
-		eta = c * tau**2 + 2 # TODO add f(x,t)
 
-		ai = sigma - 3*omega
-		bi = -2*sigma + 4*omega + eta + 1
-		ci = sigma - omega
-		di = -u[N][k]
+		ai = sum(koef_a)
+		bi = sum(koef_b)
+		ci = sum(koef_c)
+		di = -u[N][k-1]
 
 		return (ai, bi, ci, di)
+		
 	def last_eq(u):
 		"""Find coefficients of last equation"""
 		pass
@@ -145,18 +342,53 @@ class Hyperbolic_PDE(PDE):
 	def middle_eq(u):
 		"""Find coefficients of middle equation"""
 		pass
+		
+	def solve(self):
+		U = self.grid
+		print("ololo")
 
 
+##====================================================================
+## Description: parse all input
+def parse_file(f):
+	pde = PDE_parser()
+	pde.parse_pde(f.readline())
+	if pde.type == 'parabolic':
+		pde.parse_boundary_condition(f.readline())
+		pde.parse_boundary_condition(f.readline())
+		pde.parse_initial_condition(f.readline())
+		for s in f.readlines():
+			pde.parse_stuff(s)
+		return Parabolic_PDE(pde)
+
+	elif pde.type == 'hyperbolic':
+		pde.parse_boundary_condition(f.readline())
+		pde.parse_boundary_condition(f.readline())
+		pde.parse_initial_condition(f.readline())
+		pde.parse_initial_condition(f.readline())
+		for s in f.readlines():
+			pde.parse_stuff(s)
+
+	elif pde.type == 'elliptic':
+		pass
+
+	return pde
+
+
+#=====================================================================
 
 def main():
 
-	s = open("input").readline()
+	f = open("input")
 	
-#	print(parse_all(s))
+	pde = parse_file(f)
+	res = pde.solve()
+	pprint(res[0])
+	
+#	print()
 
 
 #=====================================================================
 
 if __name__ == "__main__":
 	main()
-
